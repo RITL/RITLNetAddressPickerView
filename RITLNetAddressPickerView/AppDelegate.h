2b7d1dbb84d90ab953ec2a750696de3b8498bb1c
774655048@qq.com <774655048@qq.com>
2018-04-20 16:15:47 +0800

    Initial Commit

---
+++ RITLNetAddressPickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RITLNetAddressPickerView
+//
+//  Created by YueWen on 2018/4/20.
+//  Copyright © 2018年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RITLNetAddressPickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RITLNetAddressPickerView
+//
+//  Created by YueWen on 2018/4/20.
+//  Copyright © 2018年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
